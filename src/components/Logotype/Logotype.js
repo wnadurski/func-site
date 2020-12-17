e40ed207f7a868c5c01fa6f52e3935ddedc0e415
Wojciech Nadurski <w.nadurski@gmail.com>
2020-12-17 19:43:22 +0100

    some first styling

---
+++ src/components/Logotype/Logotype.js
@@ -0,0 +1,10 @@
+import styles from "./styles.module.css"
+
+export const Logotype = () => {
+    return <div className={styles.logotype}>
+        <span className={styles.lambda}>λ</span>
+        <span className={styles.func}>func</span>
+        <span className={styles.dot}>.</span>
+        <span className={styles.site}>site</span>
+    </div>
+}
---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,7 @@
+import styles from "./styles.module.css"
+
+export const TopBar = ({children}) => {
+    return (
+        <div className={"topbar " + styles.topbar}>{children}</div>
+    )
+}
